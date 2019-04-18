89e4e8167d3fc64ab9d7827bc3efa7896108bb44
Shi Shu <ss10198@nyu.edu>
2019-04-17 23:02:49 -0400

    Began working on turns and movement

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -11,30 +12,39 @@ public class GameController : MonoBehaviour
     public CardController hand;
     public Character[] enemies;
     public Character[] allies;
+    public Queue<Character> turns;
 
     Vector3Int selectedMovementLocation;
     public Character currentCharacter;
 
     private void Start()
     {
-        currentCharacter = allies[0];
+        StartGame();
         inputControl = GetComponent<InputController>();
         hand = FindObjectOfType<CardController>();
-        StartGame();
     }
 
     // Used to start game. Potentially can run an intro before calling this function
     public void StartGame()
     {
+        // Temporary turn setup
+        turns = new Queue<Character>(allies);
+        ui.UpdateTurns(turns.ToList());
+        currentCharacter = turns.Peek();
         hand.DrawCurrentCards(currentCharacter);
         ui.SelectCharacter(currentCharacter);
-        ui.UpdateTurns(new List<Character>(allies));
     }
 
     // Used to move character from mouse input. Called from InputController
     public void MoveCharacter(Vector3 position)
     {
         currentCharacter.Move(map.FindPath(map.WorldToCellSpace(currentCharacter.transform.position), map.WorldToCellSpace(position)));
+        // Currently a player's turn ends once they move
+        var c = turns.Dequeue();
+        // Add the character back into the queue once they moved
+        turns.Enqueue(c);
+        currentCharacter = turns.Peek();
+        ui.UpdateTurns(turns.ToList());
     }
 
     //Used to select a tile from mouse input. Called from InputController.
