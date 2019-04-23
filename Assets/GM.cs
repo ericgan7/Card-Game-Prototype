a5bf6734a8c7e93341b743f6ccb8d944cae405ca
nancy10069 <nancy10069@gmail.com>
2019-04-22 22:13:12 -0400

    'MenuPage'

---
+++ Assets/GM.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VIDE_Data;
+public class GM : MonoBehaviour
+{
+
+    //some public 
+    public VIDE_Assign VA;
+    public VIDEUIManager1 diag;
+    public void startDialogue(int nodeID) {
+        //set the node id here
+
+        //
+        VA.overrideStartNode = nodeID;
+        diag.Interact(VA);
+
+
+    }
+    public static GM instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+    public void setupChar(int chara) {
+
+    }
+    public void startGame(int lv) {
+        Application.LoadLevel("Main");
+    }
+    
+    public void quit() {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (VD.isActive && Input.GetKeyUp(KeyCode.Space)){
+                diag.Interact(VA);
+            
+        }
+
+    }
+}
