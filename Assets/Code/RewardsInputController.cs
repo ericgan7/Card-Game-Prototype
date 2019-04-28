45c3f9cd7e2a54c4924df14b9c53c23296d2cd91
Shi Shu <ss10198@nyu.edu>
2019-04-27 21:27:22 -0400

    Made player portrain appear for reward selection

---
+++ Assets/Code/RewardsInputController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardsInputController : MonoBehaviour
+{
+    public RewardsController rewardsController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rewardsController.allyIndex = rewardsController.allyIndex == 0 ? 
+                rewardsController.allies.Count - 1 : (rewardsController.allyIndex - 1) % rewardsController.allies.Count;
+            rewardsController.UIController.SelectCharacter(rewardsController.allies[rewardsController.allyIndex]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rewardsController.allyIndex = (rewardsController.allyIndex + 1) % rewardsController.allies.Count;
+            rewardsController.UIController.SelectCharacter(rewardsController.allies[rewardsController.allyIndex]);
+        }
+    }
+}
